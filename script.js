24b0e02da6ebcff8468314a2381089b28d0679ef
Aldy Akbarrizky <AldyAkbarrizky18@gmail.com>
2021-09-27 19:58:38 +0700

    Merubah script.js menjadi menggunakan pendekatan OOP

--- script.js
+++ script.js
@@ -1,138 +1,125 @@
-// Convert time to a format of hours, minutes, seconds, and milliseconds
+class Timer extends HTMLElement {
+    constructor() {
+        super()
 
-function timeToString(time) {
-    let diffInHrs = time / 3600000;
-    let hh = Math.floor(diffInHrs);
+        this.playButton = document.getElementById("start-btn");
+        this.pauseButton = document.getElementById("pause-btn");
+        this.resetButton = document.getElementById("stop-btn");
+        this.lapButton = document.getElementById("lap-btn");
+        this.timerText = document.getElementById("timer");
+        this.recordText = document.getElementById("record");
 
-    let diffInMin = (diffInHrs - hh) * 60;
-    let mm = Math.floor(diffInMin);
+        this.playButton.onclick = () => this.start();
+        this.pauseButton.onclick = () => this.pause();
+        this.resetButton.onclick = () => this.reset();
+        this.lapButton.onclick = () => this.lap();
 
-    let diffInSec = (diffInMin - mm) * 60;
-    let ss = Math.floor(diffInSec);
+        this.startTime;
+        this.elapsedTime = 0;
+        this.timerInterval;
+        this.counter = 1;
+        this.prevLap;
+        this.state = "paused";
+    }
 
-    let diffInMs = (diffInSec - ss) * 100;
-    let ms = Math.floor(diffInMs);
+    timeToString(time) {
+        let diffInHrs = time / 3600000;
+        let hh = Math.floor(diffInHrs);
+    
+        let diffInMin = (diffInHrs - hh) * 60;
+        let mm = Math.floor(diffInMin);
+    
+        let diffInSec = (diffInMin - mm) * 60;
+        let ss = Math.floor(diffInSec);
+    
+        let diffInMs = (diffInSec - ss) * 100;
+        let ms = Math.floor(diffInMs);
+    
+        let formattedHH = hh.toString().padStart(2, "0");
+        let formattedMM = mm.toString().padStart(2, "0");
+        let formattedSS = ss.toString().padStart(2, "0");
+        let formattedMS = ms.toString().padStart(2, "0");
+    
+        return `${formattedHH}:${formattedMM}:${formattedSS}:${formattedMS}`;
+    }
 
-    let formattedHH = hh.toString().padStart(2, "0");
-    let formattedMM = mm.toString().padStart(2, "0");
-    let formattedSS = ss.toString().padStart(2, "0");
-    let formattedMS = ms.toString().padStart(2, "0");
-
-    return `${formattedHH}:${formattedMM}:${formattedSS}:${formattedMS}`;
-}
-
-// Declare variables to use in our functions below
-
-let startTime;
-let elapsedTime = 0;
-let timerInterval;
-let counter = 1;
-let prevLap;
-
-// Create function to modify innerHTML
-
-function print(txt) {
-    document.getElementById("timer").innerHTML = txt;
-}
-
-function printHeaderLap() {
-    // counter = 1;
-    header = "Time <br>";
-    document.getElementById("record").innerHTML = header;
-}
-
-function printLap(txt) {
-    document.getElementById("record").innerHTML += txt;
-}
-
-function printResult(hour, minutes, seconds) {  
-    if (hour != "00") {
-        var hourTime = parseInt(hour);
-        if (hourTime >= 24) {
-            var day = hourTime / 24;
-            hourTime = hourTime - 24;
-            txt = day + " hari " + toString(hourTime) + " jam " + minutes + " menit " + seconds + " detik ";            
+    printResult(hour, minutes, seconds) {  
+        let txt = "";
+        if (hour != "00") {
+            var hourTime = parseInt(hour);
+            if (hourTime >= 24) {
+                var day = parseInt(hourTime / 24);
+                hourTime = hourTime - (day * 24);
+                txt = day + " hari " + hourTime + " jam " + minutes + " menit " + seconds + " detik ";            
+            }
+            else {
+                txt = hour + " jam " + minutes + " menit " + seconds + " detik ";
+            }        
         }
-        else {
-            txt = hour + " jam " + minutes + " menit " + seconds + " detik ";
-        }        
+        else if (minutes != "00")  {
+            txt = minutes + " menit " + seconds + " detik ";
+        }
+        else if (seconds != "00")  {
+            txt = seconds + " detik ";
+        } else {
+            txt = "0 detik"
+        }
+        return txt
     }
-    else if (minutes != "00")  {
-        txt = minutes + " menit " + seconds + " detik ";
+
+    start() {
+        this.startTime = Date.now() - this.elapsedTime
+        if(this.counter === 1) {
+            this.recordText.innerHTML = "Time <br/>"
+        }
+        this.timerInterval = setInterval(() => {
+            this.elapsedTime = Date.now() - this.startTime;
+            this.timerText.innerHTML = this.timeToString(this.elapsedTime);
+        }, 10);
+
+        this.state = "started";
+        this.pauseButton.style.display = "inline-block";
+        this.playButton.style.display = "none";
+        this.lapButton.disabled = false;
     }
-    else if (seconds != "00")  {
-        txt = seconds + " detik ";
-    } else {
-        txt = "0 detik"
+
+    pause() {
+        clearInterval(this.timerInterval);
+
+        this.state = "paused";
+        this.pauseButton.style.display = "none";
+        this.playButton.style.display = "inline-block";
+        this.lapButton.disabled = true;
     }
-    return txt
-}
-// Create "start", "pause" and "reset" functions
 
-function start() {
-    startTime = Date.now() - elapsedTime;
-    if (counter === 1) {
-        printHeaderLap();
-        prevLap = startTime;
+    reset() {
+        const str = this.timerText.innerHTML;
+        const timeArr = str.split(":");
+        const final_time = "Final time: " + this.printResult(timeArr[0], timeArr[1], timeArr[2]) + "<br>";
+        this.recordText.innerHTML += final_time
+        clearInterval(this.timerInterval);
+        this.timerText.innerHTML = "00:00:00:00";
+        this.elapsedTime = 0;
+
+        this.state = "paused";
+        this.pauseButton.style.display = "none";
+        this.playButton.style.display = "inline-block";
+        this.lapButton.disabled = true;
     }
-    timerInterval = setInterval(function printTime() {
-        elapsedTime = Date.now() - startTime;
-        print(timeToString(elapsedTime));
-        localStorage.setItem("time", elapsedTime);
-    }, 10);
-    showButton("PAUSE");
+
+    lap() {
+        const currTime = this.startTime - this.elapsedTime;
+        let lapTime = "";
+        if(this.counter == 1) {
+            lapTime = this.elapsedTime;
+        } else {
+            lapTime = this.prevLap - currTime;
+        }
+        const lap_time = this.counter + " " + this.timerText.innerHTML + " (+" + this.timeToString(lapTime) + ")<br>";
+        this.recordText.innerHTML += lap_time;
+        this.prevLap = currTime;
+        this.counter += 1;
+    }
 }
 
-function pause() {
-    clearInterval(timerInterval);
-    showButton("PLAY");
-}
-
-function reset() {
-    currTime = startTime - elapsedTime;
-    lapTime = prevLap - currTime;
-    str = document.getElementById("timer").innerHTML
-    const timeArr = str.split(":");
-    lap = "Final time: " + printResult(timeArr[0], timeArr[1], timeArr[2]) + "<br>";
-    printLap(lap);
-    clearInterval(timerInterval);
-    print("00:00:00:00");
-    elapsedTime = 0;
-    localStorage.setItem("time", 0);
-    showButton("PLAY");
-}
-
-function lap() {
-    currTime = startTime - elapsedTime;
-    lapTime = prevLap - currTime;
-    lap = counter + " " + document.getElementById("timer").innerHTML + " (+" + timeToString(lapTime) + ")<br>";
-    printLap(lap);
-    prevLap = currTime;
-    counter += 1;
-}
-
-// Create function to display buttons
-
-function showButton(buttonKey) {
-    playButton.style.display = buttonKey === "PLAY" ? "inline-block" : "none";
-    pauseButton.style.display = buttonKey === "PLAY" ? "none" : "inline-block";
-    lapButton.disabled = buttonKey === "PLAY" ? true : false;
-}
-// Create event listeners
-
-function load() {
-    elapsedTime = localStorage.getItem("time");
-    print(timeToString(elapsedTime));
-}
-
-window.onbeforeunload = load();
-
-let playButton = document.getElementById("start-btn");
-let pauseButton = document.getElementById("pause-btn");
-let resetButton = document.getElementById("stop-btn");
-let lapButton = document.getElementById("lap-btn");
-
-playButton.addEventListener("click", start);
-pauseButton.addEventListener("click", pause);
-resetButton.addEventListener("click", reset);
-lapButton.addEventListener("click", lap);
+window.customElements.define("the-timer", Timer)
